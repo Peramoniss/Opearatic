77a45e7d3bd8716c2d58eec49853282b881cb1bc
Peramonis <dudueberhardtpereira@gmail.com>
2026-01-05 12:31:36 -0300

    allow localhosts

--- api/src/app.py
+++ api/src/app.py
@@ -9,7 +9,13 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://peramoniss.github.io/Opearatic/"], 
+    allow_origins=[
+        "https://peramoniss.github.io",
+        "http://localhost:3000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:8000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
